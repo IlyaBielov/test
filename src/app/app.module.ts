7e59a0c1bcb3623ce6d211995af807871ec401be
IlyaBielov <ibioriginalibi@gmail.com>
2020-01-09 15:25:26 +0200

    create feed-card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FeedCardComponent } from './components/feed-card/feed-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedCardComponent
   ],
   imports: [
     BrowserModule,
